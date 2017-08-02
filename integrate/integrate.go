bd71c7df946bc4f672f7e3e097b52cedc043f754
hambrle <dzanan_ganic@hotmail.com>
2017-08-02 20:31:42 +0200

    removed integrate.go (currently unnecessary)

--- integrate/integrate.go
+++
@@ -1,9 +0,0 @@
-package integrate
-
-// package integrate
-
-// type integrationFunc func(func(float64) float64, float64, float64, float64, float64) float64
-
-// func IntegrateByAccuracy() (float64, error) {
-
-// }
